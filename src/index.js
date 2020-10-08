d9d11a29f744adeae3900ec97e0372364f999c29
Home <praveenmatthewphilip@gmail.com>
2020-10-08 23:44:29 +0530

    final version without style

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1 className="title">Update with container</h1>
+        <h1 className="title">React Movie Search</h1>
         <SearchMovies />
       </div>
     )
--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,20 +1,62 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function () {
+  const [query, setQuery] = useState("")
+  const [movies, setMovies] = useState([])
+  const searchMovies = async (e) => {
+    e.preventDefault()
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=d13e6e25b691fd35965ada1e535378d6&language=en-US&query=${query}&page=1&include_adult=true`
+    try {
+      const res = await fetch(url)
+      const data = await res.json()
+      data.results && setMovies(data.results)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
-    <form className="form">
-      <label className="label" htmlFor="query">
-        Movie Name
-      </label>
-      <input
-        className="input"
-        type="text"
-        name="query"
-        placeholder="i.e. Jurassic Park"
-      />
-      <button className="button" type="submit">
-        Search
-      </button>
-    </form>
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="i.e. Jurassic Park"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <div className="card" key={movie.id}>
+              <img
+                className="card--image"
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <div className="card--content">
+                <h3 className="card--title">{movie.title}</h3>
+                <p>
+                  <small>RELEASE DATE: {movie.release_date}</small>
+                </p>
+                <p>
+                  <small>RATING : {movie.vote_average}</small>
+                </p>
+                <p className="card--desc">{movie.overview}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
   )
 }
